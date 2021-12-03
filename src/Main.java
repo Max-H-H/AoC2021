053621ae9aa5e96f0a8ccc2c9f57c319dda11e28
Max <Max.Hvid-Hansen@Hotmail.com>
2021-12-03 23:58:40 +0100

    Day 1 added

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,15 @@
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        //Answer for Day1 Puzzle 1
+    //System.out.println(Day1.incHigher(Day1.stringToIntArray(Day1.input1)));
+
+        //Answer for Day1 Puzzle 2
+    //System.out.print((Day1.incHigher(Day1.intArrayToThreeSumArray(Day1.stringToIntArray(Day1.input1)))));
+
+
+
     }
 }
