dc32ea2f81965500626b6ce4f6ef85c1052b56af
Max <Max.Hvid-Hansen@Hotmail.com>
2021-12-04 12:07:01 +0100

    Added Day2

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,10 @@ public class Main {
         //Answer for Day1 Puzzle 2
     //System.out.print((Day1.incHigher(Day1.intArrayToThreeSumArray(Day1.stringToIntArray(Day1.input1)))));
 
+        //Answer for Day 2 Puzzle 1
+    //System.out.print(Day2.directionCalculator(Day2.stringToStringArray(Day2.input2)));
 
-
+        //Answer for Day2 Puzzle 2
+    //System.out.print(Day2.aimCalculator(Day2.stringToStringArray(Day2.input2)));
     }
 }
